5b7a5f3258baec49b8186019c7c49a652b3e7fdc
Brian W. Wolter <brianwolter@gmail.com>
2023-07-30 18:56:22 -0400

    ...

--- src/error.rs
+++ src/error.rs
@@ -2,18 +2,36 @@ use std::io;
 use std::fmt;
 
 use keyring;
+use argon2;
+use chacha20poly1305;
+use crypto_common;
+use sled;
 
 #[derive(Debug)]
 pub enum Error {
   IOError(io::Error),
+  SledError(sled::Error),
   KeyringError(keyring::Error),
+  Argon2Error(argon2::Error),
+  DeriveKeyError(argon2::password_hash::Error),
+  InvalidLength(crypto_common::InvalidLength),
+  CipherError(chacha20poly1305::Error),
+  InvalidPassword,
+	VersionMismatch,
   PasswordMismatch,
   PasswordEmpty,
+  NoSuchDirectory,
 }
 
 impl From<io::Error> for Error {
   fn from(err: io::Error) -> Self {
     Self::IOError(err)
+  }
+}
+
+impl From<sled::Error> for Error {
+  fn from(err: sled::Error) -> Self {
+    Self::SledError(err)
   }
 }
 
@@ -23,13 +41,45 @@ impl From<keyring::Error> for Error {
   }
 }
 
+impl From<argon2::Error> for Error {
+  fn from(err: argon2::Error) -> Self {
+    Self::Argon2Error(err)
+  }
+}
+
+impl From<argon2::password_hash::Error> for Error {
+  fn from(err: argon2::password_hash::Error) -> Self {
+    Self::DeriveKeyError(err)
+  }
+}
+
+impl From<crypto_common::InvalidLength> for Error {
+  fn from(err: crypto_common::InvalidLength) -> Self {
+    Self::InvalidLength(err)
+  }
+}
+
+impl From<chacha20poly1305::Error> for Error {
+  fn from(err: chacha20poly1305::Error) -> Self {
+    Self::CipherError(err)
+  }
+}
+
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
       Self::IOError(err) => err.fmt(f),
+      Self::SledError(err) => err.fmt(f),
       Self::KeyringError(err) => err.fmt(f),
+      Self::Argon2Error(err) => err.fmt(f),
+      Self::DeriveKeyError(err) => err.fmt(f),
+      Self::InvalidLength(err) => err.fmt(f),
+      Self::CipherError(err) => err.fmt(f),
+      Self::InvalidPassword => write!(f, "Invalid password"),
+      Self::VersionMismatch => write!(f, "Version mismatch"),
       Self::PasswordMismatch => write!(f, "Passwords do not match"),
       Self::PasswordEmpty => write!(f, "Password is empty"),
+      Self::NoSuchDirectory => write!(f, "No such directory"),
     }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use std::process;
+use std::path;
 
 use clap::Parser;
 use colored::Colorize;
+
+use sled;
+use dirs;
+
 use keyring;
 use rpassword;
+use chacha20poly1305::{self, ChaCha20Poly1305, KeyInit, AeadCore, aead::{Aead, OsRng}};
+use argon2::{self, PasswordHasher};
 
 mod error;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 const KEYRING_TARGET: &str = "User";
-const DEFAULT_STORE: &str = "~/.coolvs/store.db";
+const DEFAULT_STORE: &str = ".coolvs/store.db";
 
 #[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
@@ -33,33 +41,78 @@ fn main() {
 
 fn cmd() -> Result<(), error::Error> {
   let opts = Options::parse();
-  let store = match opts.store {
-    Some(store) => store,
-    None => DEFAULT_STORE.to_owned(),
+  let store: path::PathBuf = match opts.store {
+    Some(store) => path::PathBuf::from(&store),
+    None => default_store()?,
   };
-  
-	let mut entry = keyring::Entry::new_with_target(KEYRING_TARGET, "coolvs.brianwolter.com", &store)?;
-  let passwd = match entry.get_password() {
-    Ok(passwd) => passwd,
+
+	let mut entry = keyring::Entry::new_with_target(KEYRING_TARGET, "coolvs.brianwolter.com", &store.display().to_string())?;
+  let (passwd, key) = match entry.get_password() {
+    Ok(passwd) => (passwd.clone(), derive_key(&passwd)?),
     Err(err) => match err {
-      keyring::Error::NoEntry => set_password(&store, &mut entry)?,
+      keyring::Error::NoEntry => collect_password(&store, &mut entry)?,
       _ => return Err(err.into()),
     },
   };
-  
-  println!(">>> {:?}", passwd);
+
+	let db = sled::open(store)?;
+	let data = db.open_tree("data")?;
+	let meta = db.open_tree("meta")?;
+
+	match meta.get("version")? {
+		Some(v) => if v != VERSION { return Err(error::Error::VersionMismatch) },
+		None => { meta.insert("version", VERSION)?; },
+	};
+
+	let cipher = ChaCha20Poly1305::new_from_slice(&key)?;
+	let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
+	
+	let enc = cipher.encrypt(&nonce, "Cool, this is the message".as_ref())?;
+	let dec = cipher.decrypt(&nonce, enc.as_ref())?;
+	println!(">>> DEC DEC DEC {:?}", dec);
+
   Ok(())
 }
 
-fn set_password(store: &str, entry: &mut keyring::Entry) -> Result<String, error::Error> {
-  println!("[{}]", store);
-  let pass = rpassword::prompt_password("New password for store: ")?;
+fn default_store() -> Result<path::PathBuf, error::Error> {
+	let mut home = match dirs::home_dir() {
+		Some(home) => home,
+		None => return Err(error::Error::NoSuchDirectory),
+	};
+	home.push(DEFAULT_STORE);
+	Ok(home)
+}
+
+fn derive_key(passwd: &str) -> Result<[u8; 32], error::Error> {
+	let hash = argon2::password_hash::PasswordHash::new(&passwd)?;
+	let passwd = match hash.hash {
+		Some(passwd) => passwd,
+		None => return Err(error::Error::InvalidPassword),
+	};
+	let salt = match hash.salt {
+		Some(salt) => salt,
+		None => return Err(error::Error::InvalidPassword),
+	};
+	let mut key = [0u8; 32]; // Can be any desired size
+	argon2::Argon2::default().hash_password_into(passwd.as_bytes(), salt.as_str().as_bytes(), &mut key)?;
+	Ok(key)
+}
+
+fn collect_password<P: AsRef<path::Path>>(store: P, entry: &mut keyring::Entry) -> Result<(String, [u8; 32]), error::Error> {
+  println!("[{}]", store.as_ref().to_string_lossy());
+
+	let pass = rpassword::prompt_password("New password for store: ")?;
   if pass.len() == 0 {
     return Err(error::Error::PasswordEmpty);
   }
   if pass != rpassword::prompt_password("   That password again: ")? {
     return Err(error::Error::PasswordMismatch);
   }
-  entry.set_password(&pass)?;
-  Ok(pass)
+	
+	let salt = argon2::password_hash::SaltString::generate(&mut argon2::password_hash::rand_core::OsRng);
+	let passwd = argon2::Argon2::default().hash_password(pass.as_bytes(), &salt)?.to_string();
+	let key = derive_key(&passwd)?;
+
+  entry.set_password(&passwd)?;
+  Ok((passwd, key))
 }
